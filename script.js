0db8c3150960b90303e5c646c3120bae938d3d6f
Tom <noppanat.sripan@gmail.com>
2024-12-04 02:33:31 -0800

    add post form

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
+
     const input = document.querySelector('input');
 
     input.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
             const target_content = event.target.value.trim();
-            
-            console.log(target_content);
-
-            
-
-
-
-
-
-
+            console.log("Entered content:", target_content);
         }
     });
+
+    
+    const sendFrom = (event) => {
+        event.preventDefault(); 
+        const form = document.getElementById("post-form");
+        const title = form.querySelector('input[name="title"]').value.trim();
+        const comment = form.querySelector('input[name="comment"]').value.trim();
+
+        console.log("Title:", title);
+        console.log("Comment:", comment);
+        const myDiv = document.getElementById("post-status");
+        const newParagraph = document.createElement('p');
+        newParagraph.textContent = `added title: ${title} comment: ${comment}`;
+        myDiv.appendChild(newParagraph);
+        
+        setTimeout(() => {
+            newParagraph.remove();
+        }, 3000); 
+
+        form.reset();
+        
+
+    
+    };
+
+
+    document.querySelector('#post-form input[type="button"]').onclick = sendFrom;
 });
